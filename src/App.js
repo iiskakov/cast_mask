d7ac4348b6c67220763eb4ecf494897e56c2fe3f
Iskander <tradeshells@gmail.com>
2023-03-01 14:23:53 +0800

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function App() {
   const { data, error, isLoading } = useSWR(
-    "https://api.github.com/repos/vercel/swr",
+    "https://cast-parser.onrender.com/api/parse_details?url=https://cast.kz/list/Ar4Eil/1234",
     fetcher
   );
 
@@ -14,10 +14,12 @@ export default function App() {
   return (
     <div>
       <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👁 {data.subscribers_count}</strong>{" "}
-      <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong>
+      <h1>{data}</h1>
+      <h1>{data['Город:']}</h1>
+      {/* <p>{data.description}</p> */}
+      {/* <strong>👁 {data.subscribers_count}</strong>{" "} */}
+      {/* <strong>✨ {data.stargazers_count}</strong>{" "} */}
+      {/* <strong>🍴 {data.forks_count}</strong> */}
     </div>
   );
 }
