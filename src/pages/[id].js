9224081903879e3395bbd1380c258765a583d5a3
Iskander <tradeshells@gmail.com>
2023-03-01 16:18:19 +0800

    test

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -4,12 +4,14 @@ import { useRouter } from "next/router";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function Single() {
+export default function App() {
   const router = useRouter();
-  const { URL } = router.query;
+  const { id } = router.query;
+  const baseUrl = "https://cast.kz/list/";
+  const formattedId = id?.startsWith("/") ? id.substring(1) : id;
 
   const { data, error, isLoading } = useSWR(
-    `https://cast-parser.onrender.com/api/parse_details?url=${URL}`,
+    `${baseUrl}${formattedId}`,
     fetcher
   );
 
@@ -17,7 +19,6 @@ export default function Single() {
   if (isLoading) return "Loading...";
   return (
     <div>
-      <h1>{data["Cast.kz ID"]}</h1>
       <h1>{data["Город"]}</h1>
       <h1>{data["Пол"]}</h1>
 
